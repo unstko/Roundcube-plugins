7695a93bdeaeacd53c1d87df055880f800ad24aa
Stefan Koch <unstko@gmx.net>
2011-08-05 19:42:03 +0200

    Add character limiting functionality
    Create event listener for Roundcube.
    Add character limiting function to message text area.
    Add an input field to show left characters.

--- shortmail_character_limiter/shortmail_character_limiter.js
+++ shortmail_character_limiter/shortmail_character_limiter.js
@@ -1,3 +1,39 @@
+if (window.rcmail) {
+    rcmail.addEventListener('init', function(evt) {
+        // Character limit
+        var limit = 5;
+
+        // Append field to show left characters
+        $('#compose-buttons').append
+        (
+            $('<span>')
+                .attr('id', 'shortmail_character_limiter_field_left')
+                .html
+                (
+                    $('<input>')
+                        .attr('readonly', 'readonly')
+                        .attr('type', 'text')
+                        .attr('name', 'shortmail_character_limiter_field_left')
+                        .attr('size', '3')
+                        .attr('maxlength', '3')
+                        .attr('value', limit)
+                )
+        );
+
+        // Bind keyup and keydown event handler to message text area
+        $('#compose-body').bind('keyup keydown', function() {
+            if (document.form._message.value.length > limit) {
+                // If too long, then trim it
+                document.form._message.value = document.form._message.value.substring(0, limit);
+            }
+            else {
+                // Otherwise update characters left counter
+                document.form.shortmail_character_limiter_field_left.value = limit - document.form._message.value.length;
+            }
+        });
+    });
+}
+
 /**
  * Limit the number of characters per textarea.
  * Use one function for multiple text areas on a page.
